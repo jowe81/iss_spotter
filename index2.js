b541119bf966115ff29ee0046c23a070abf01771
Johannes Weber <johannes@drweber.de>
2021-11-13 14:03:30 -0800

    cleanup

--- index2.js
+++ index2.js
@@ -2,4 +2,4 @@ const { nextISSTimesForMyLocation } = require('./iss_promised');
 const { printPassTimes } = require('./printPassTimes');
 
 nextISSTimesForMyLocation()
-  .then(printPassTimes);
+  .then(printPassTimes); //Again, .then((passTimes) => {printPassTimes(passTimes);}) shortened to just the function name
