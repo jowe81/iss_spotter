643d8ebc4fb02ea9e2edc9f7cf0fae364f955e21
Johannes Weber <johannes@drweber.de>
2021-11-12 18:10:46 -0800

    error handling

--- iss.js
+++ iss.js
@@ -5,8 +5,16 @@ const { fetchMyIP } = require('./fetchip');
 const fetchCoordsByIp = (ip, callback) => {
   const requestUrl = constants.FREE_GEO_IP.API_ENDPOINT + ip + '?' + constants.FREE_GEO_IP.API_KEY;
   request(requestUrl, (err, res, body) => {
-    const { latitude, longitude } = JSON.parse(body);
-    callback(err, { latitude, longitude });
+    if (!err) {
+      if (res.statusCode === 200) {
+        const { latitude, longitude } = JSON.parse(body);
+        callback(null, { latitude, longitude });
+      } else {
+        callback(new Error(`Status Code ${res.statusCode} when fetching coordinates for IP. Response: ${body}`));
+      }
+    } else {
+      callback(new Error(`Request to ${constants.FREE_GEO_IP.API_ENDPOINT} failed. Returned error: ${err}`));
+    }
   });
 };
 
