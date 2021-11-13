6bcb752d56b30bd1dffa722829a8155dd0def70e
Johannes Weber <johannes@drweber.de>
2021-11-13 09:43:01 -0800

    printPassTimes(), output formatting

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const { fetchMyIP } = require('./fetchip');
-const { fetchCoordsByIp, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = (passTimes) => {
+  for (let passTime of passTimes) {
+    console.log(`Next pass at ${new Date(passTime.risetime * 1000).toString()} for ${passTime.duration} seconds!`);
+  }
+};
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  console.log(passTimes);
+  printPassTimes(passTimes);
 });
