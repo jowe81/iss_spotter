464ce6a9e039ca6e1e31f8d6c5fd04a546aeef34
Johannes Weber <johannes@drweber.de>
2021-11-13 11:40:57 -0800

    Add IPIFY

--- constants.js
+++ constants.js
@@ -1,5 +1,9 @@
 const constants = {
 
+  IPIFY: {
+    API_ENDPOINT: "https://api.ipify.org?format=json",
+  },
+
   FREE_GEO_IP: {
     API_KEY: "apikey=d9906440-4422-11ec-b874-3700ba292615",
     API_ENDPOINT: "https://api.freegeoip.app/json/",
