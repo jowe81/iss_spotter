ce47e0a1695f2b34cd0f82ac1e3fc496a1846ded
Johannes Weber <johannes@drweber.de>
2021-11-13 14:07:36 -0800

    error handling added

--- index2.js
+++ index2.js
@@ -2,4 +2,7 @@ const { nextISSTimesForMyLocation } = require('./iss_promised');
 const { printPassTimes } = require('./printPassTimes');
 
 nextISSTimesForMyLocation()
-  .then(printPassTimes); //Again, .then((passTimes) => {printPassTimes(passTimes);}) shortened to just the function name
+  .then(printPassTimes) //Again, .then((passTimes) => {printPassTimes(passTimes);}) shortened to just the function name
+  .catch(error => {
+    console.log("It didn't work: ", error.message);
+  });
