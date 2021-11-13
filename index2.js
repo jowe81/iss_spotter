523c7526d5a58893fe5b4d98f40be15c79d06ebb
Johannes Weber <johannes@drweber.de>
2021-11-13 14:02:01 -0800

    remove printPassTimes from here

--- index2.js
+++ index2.js
@@ -1,12 +1,5 @@
 const { nextISSTimesForMyLocation } = require('./iss_promised');
 const { printPassTimes } = require('./printPassTimes');
 
-//Copied from index.js
-const printPassTimes = (passTimes) => {
-  for (let passTime of passTimes) {
-    console.log(`Next pass at ${new Date(passTime.risetime * 1000).toString()} for ${passTime.duration} seconds!`);
-  }
-};
-
 nextISSTimesForMyLocation()
   .then(printPassTimes);
